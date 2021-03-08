e8d0eb0f77de22013b1919800d8661c97fce3ee4
German Enik <germanenik@gmail.com>
2021-03-07 19:37:35 -0700

    edits for training on our own data

--- PreSumm/src/prepro/data_builder.py
+++ PreSumm/src/prepro/data_builder.py
@@ -330,23 +330,30 @@ def _format_to_bert(params):
 
 def format_to_lines(args):
     corpus_mapping = {}
-    for corpus_type in ['valid', 'test', 'train']:
-        temp = []
-        for line in open(pjoin(args.map_path, 'mapping_' + corpus_type + '.txt')):
-            temp.append(hashhex(line.strip()))
-        corpus_mapping[corpus_type] = {key.strip(): 1 for key in temp}
-    breakpoint()
+    # for corpus_type in ['valid', 'test', 'train']:
+    #     temp = []
+    #     for line in open(pjoin(args.map_path, 'mapping_' + corpus_type + '.txt')):
+    #         temp.append(hashhex(line.strip()))
+    #     corpus_mapping[corpus_type] = {key.strip(): 1 for key in temp}
+    
     train_files, valid_files, test_files = [], [], []
-    for f in glob.glob(pjoin(args.raw_path, '*.json')):
-        real_name = f.split('/')[-1].split('.')[0]
-        if (real_name in corpus_mapping['valid']):
-            valid_files.append(f)
-        elif (real_name in corpus_mapping['test']):
-            test_files.append(f)
-        elif (real_name in corpus_mapping['train']):
-            train_files.append(f)
-        # else:
+    paths = glob.glob(pjoin(args.raw_path, '*.json'))
+    total_num = len(paths)
+    random.shuffle(paths)
+    b1, b2 = total_num * 3 // 5, total_num * 4 // 5 
+    train_files = paths[:b1] #60%
+    valid_files = paths[b1:b2] #20%
+    test_files = paths[b2:] #20%
+        # real_name = f.split('/')[-1].split('.')[0]
+        # if (real_name in corpus_mapping['valid']):
+        #     valid_files.append(f)
+        # elif (real_name in corpus_mapping['test']):
+        #     test_files.append(f)
+        # elif (real_name in corpus_mapping['train']):
         #     train_files.append(f)
+        # # else:
+        # #     train_files.append(f)
+
 
     corpora = {'train': train_files, 'valid': valid_files, 'test': test_files}
     for corpus_type in ['train', 'valid', 'test']:
