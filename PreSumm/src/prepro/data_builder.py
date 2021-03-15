f18dff508a6d2ef5b6820aa57d0dd640214681d9
German Enik <germanenik@gmail.com>
2021-03-14 23:05:14 -0600

    assign augmented data only to train set

--- PreSumm/src/prepro/data_builder.py
+++ PreSumm/src/prepro/data_builder.py
@@ -337,12 +337,15 @@ def format_to_lines(args):
     
     train_files, valid_files, test_files = [], [], []
     paths = glob.glob(pjoin(args.raw_path, '*.json'))
-    total_num = len(paths)
-    random.shuffle(paths)
+    junk_paths = [path for path in paths if "random" in path]
+    original_paths = [path for path in paths if "random" not in path]
+    total_num = len(original_paths)
+    random.shuffle(original_paths)
     b1, b2 = total_num * 3 // 5, total_num * 4 // 5 
-    train_files = paths[:b1] #60%
-    valid_files = paths[b1:b2] #20%
-    test_files = paths[b2:] #20%
+    train_files = original_paths[:b1] #60%
+    train_files += junk_paths
+    valid_files = original_paths[b1:b2] #20%
+    test_files = original_paths[b2:] #20%
         # real_name = f.split('/')[-1].split('.')[0]
         # if (real_name in corpus_mapping['valid']):
         #     valid_files.append(f)
