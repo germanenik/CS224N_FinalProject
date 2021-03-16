95389b5536ce0370cd18b8eb6b07c1b43b6cb67e
varun-tandon <varun@VM1.tppnrafzh3gehpau35yegq1ovc.bx.internal.cloudapp.net>
2021-03-16 18:36:07 +0000

    added new results

--- access/scripts/train.py
+++ access/scripts/train.py
@@ -15,47 +15,47 @@ from glob import glob
 from shutil import rmtree
 
 if __name__ == '__main__':
-    # while(True):
-    #     orig_stdout = sys.stdout
-    #     new_file = uuid.uuid4()
-    #     print(new_file)
-    #     orig_stdout = sys.stdout
-    #     f = open(f'{new_file}.txt', 'w+')
-    #     sys.stdout = f
-    print('Training a model from scratch')
-    prepare_wikilarge()
-    prepare_turkcorpus()
-    random_kwargs = {
-        'arch': 'transformer',
-        'warmup_updates': 4000,
-        'parametrization_budget': 256,
-        'beam': random.randint(2, 10),
-        'dataset': 'simplification',
-        'dropout': random.random(),
-        'fp16': False,
-        'label_smoothing': random.random(),
-        'lr': random.random() * (10.0 ** random.randint(-5, -3)),
-        'lr_scheduler': 'fixed',
-        'max_epoch': 100,
-        'max_tokens': 5000,
-        'metrics_coefs': [0, 1, 0],
-        'optimizer': 'adam',
-        'validations_before_sari_early_stopping': 10,
-        'preprocessors_kwargs': {
-            'LengthRatioPreprocessor': {
-                'target_ratio': random.random()  # Default initial value
-            },
-            'LevenshteinPreprocessor': {
-                'target_ratio': random.random()  # Default initial value
-            },
-            'WordRankRatioPreprocessor': {
-                'target_ratio': random.random()  # Default initial value
-            },
-            'DependencyTreeDepthRatioPreprocessor': {
-                'target_ratio': random.random()  # Default initial value
-            },
-            'SentencePiecePreprocessor': {
-                'vocab_size': 10000
+    while(True):
+        new_file = uuid.uuid4()
+        print(new_file)
+        orig_stdout = sys.stdout
+        f = open(f'{new_file}.txt', 'w+')
+        sys.stdout = f
+        print('Training a model from scratch')
+        prepare_wikilarge()
+        prepare_turkcorpus()
+        random_kwargs = {
+            'arch': 'transformer',
+            'warmup_updates': 4000,
+            'parametrization_budget': 64,
+            'beam': random.randint(2, 10),
+            'dataset': 'simplification',
+            'dropout': random.random(),
+            'fp16': False,
+            'label_smoothing': random.random(),
+            'lr': random.random() * (10.0 ** random.randint(-5, -3)),
+            'lr_scheduler': 'fixed',
+            'max_epoch': 100,
+            'max_tokens': 5000,
+            'metrics_coefs': [0, 1, 0],
+            'optimizer': 'adam',
+            'validations_before_sari_early_stopping': 10,
+            'preprocessors_kwargs': {
+                'LengthRatioPreprocessor': {
+                    'target_ratio': random.random()  # Default initial value
+                },
+                'LevenshteinPreprocessor': {
+                    'target_ratio': random.random()  # Default initial value
+                },
+                'WordRankRatioPreprocessor': {
+                    'target_ratio': random.random()  # Default initial value
+                },
+                'DependencyTreeDepthRatioPreprocessor': {
+                    'target_ratio': random.random()  # Default initial value
+                },
+                'SentencePiecePreprocessor': {
+                    'vocab_size': 10000
+                }
             }
         }
     }
